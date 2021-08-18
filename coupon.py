4b54de1d5d136955f2fb23bc0e85a350cd2abc06
jorgehermo9 <jorgehermogonzalez@gmail.com>
2021-08-18 18:24:22 +0200

    update

--- coupon.py
+++ coupon.py
@@ -1,7 +1,61 @@
 import requests
+import time
+import os
+from dotenv import load_dotenv
 
-URL ="https://swq.jp/l/en-US/"
+load_dotenv()
 
-page = requests.get(URL)
+class Resource:
+    def __init__(self,data):
+        self.quantity=data["Quantity"]
+        self.label = data["Sw_Resource"]["Label"]
+    def __repr__(self):
+        return "x"+self.quantity + " " + self.label
 
-print(page.text)
+class Coupon:
+    def __init__(self,data):
+        self.label = data["Label"]
+        self.status = data["Status"]
+        self.resources = [Resource(item) for item in data["Resources"]]
+    def __repr__(self):
+        message=""
+        message+= "Coupon: "+self.label+"\n\n"
+        message+="http://withhive.me/313/"+self.label+"\n\n"
+        message+="Resources:\n"
+        for resource in self.resources:
+            message+=resource.__repr__() + "\n"
+        return message
+
+
+class Bot:
+    def __init__(self):
+        self.URL = "https://swq.jp/_special/rest/Sw/Coupon"
+        self.chatURL = "https://api.telegram.org/bot"+os.environ["BOT_TOKEN"]+"/sendMessage"
+        response = requests.get(self.URL)
+        data = response.json()["data"]
+        self.coupons = [Coupon(item) for item in data[1:] if item["Status"]=="verified"]
+    def notify(self,coupons):
+        for coupon in coupons:
+            dataObj={
+                "chat_id":os.environ["CHAT_ID"],
+                "text": coupon.__repr__()
+            }
+            requests.post(self.chatURL,data=dataObj)
+            print(coupon)
+
+    def run(self):
+        while True:
+            response = requests.get(self.URL)
+            data = response.json()["data"]
+            coupons = [Coupon(item) for item in data if item["Status"]=="verified"]
+            oldLabels =[item.label for item in self.coupons]
+            newCoupons =[item for item in coupons if item.label not in oldLabels]
+            if(len(newCoupons)>0):
+                self.notify(newCoupons)
+            self.coupons=coupons
+            time.sleep(900)
+
+
+if __name__ == "__main__":
+    os.environ["BOT_TOKEN"]
+    Bot().run()
